b002096c3a4448362bb85c0a1f28941f7173b9cf
Carlos Adrian <Teshynil@users.noreply.github.com>
2019-04-29 01:33:06 -0500

    core dependencies
    dependencias basicas para trabajar

---
+++ src/Controller/AuthenticationController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthenticationController extends AbstractController
+{
+    
+    public function index()
+    {
+        return $this->render('authentication/index.html.twig', [
+            'controller_name' => 'AuthenticationController',
+        ]);
+    }
+}
