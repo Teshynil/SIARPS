6b6237740b6c50870f9e483e7e0fb4af191e9373
Carlos Adrian <Teshynil@users.noreply.github.com>
2019-07-10 11:10:49 -0500

    correction

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class DashboardController extends AbstractController {
+
+    public function index(SessionInterface $session) {
+        $session->set("notifications", [
+            ["text" => "hola mundo",
+                "icon" => "notebook",
+                "color" => "success",
+                "path" => "notifications",
+                "parameters" => ["hola" => "dwa"]
+            ]
+        ]);
+        return $this->render('dashboard.html.twig');
+    }
+
+}
